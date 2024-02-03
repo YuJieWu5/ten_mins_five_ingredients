b9c2465ba85ad9c2242a229d57b082ae20e2cfc5
YuJie Wu <wuvivian725@gmail.com>
2024-02-02 18:14:06 -0800

    Create log in page

---
+++ lib/log_in.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class LogInPage extends StatefulWidget {
+  const LogInPage({super.key});
+
+  @override
+  State<LogInPage> createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _userNameController =  TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _onLoginPressed(){
+    if(_formKey.currentState?.validate() ?? false) {
+      //TODO: save account info to database
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          title: Center(
+              child: Text('Sign Up', style: TextStyle(color: Theme.of(context).colorScheme.onPrimary))
+          ),
+        ),
+        body: SingleChildScrollView(
+            child: Form(
+                key: _formKey,
+                child: SafeArea(
+                  child: Column(
+                    children: [
+                      Container(
+                          padding: const EdgeInsets.only(top: 20.0, bottom: 10.0),
+                          child: Center(
+                              child: SizedBox(
+                                  width: 400.0,
+                                  child: TextFormField(
+                                    decoration: const InputDecoration(
+                                        labelText: 'Username'
+                                    ),
+                                    controller: _userNameController,
+                                    validator: (newValue) {
+                                      if(newValue == null || newValue.isEmpty) {
+                                        return 'Username must not be blank.';
+                                      }
+                                      return null;
+                                    },
+                                  )
+                              )
+                          )
+                      ),
+                      SizedBox(
+                        width: 400.0,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                              labelText: 'Password'
+                          ),
+                          controller: _passwordController,
+                          validator: (newValue) {
+                            if(newValue == null || newValue.isEmpty) {
+                              return 'Password must not be blank.';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Container(
+                          margin: const EdgeInsets.only(top:20.0),
+                          child: ElevatedButton(
+                            // Note: we are not calling _onSavePressed! We are passing it
+                            // like an object to this other widget as a constructor arg.
+                              onPressed: _onLoginPressed,
+                              child: const Text('Create')
+                          )
+                      )
+                    ],
+                  ),
+                )
+            )
+        )
+    );
+  }
+}
