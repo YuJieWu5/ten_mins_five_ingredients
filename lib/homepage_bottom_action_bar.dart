f4b24cac54897c2b763310f6af7bb2ac180b1771
YuJie Wu <wuvivian725@gmail.com>
2024-02-05 13:05:15 -0800

    Add constructor on homepage_bottom_action_bar.dart

--- lib/homepage_bottom_action_bar.dart
+++ lib/homepage_bottom_action_bar.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class BottomActionBar extends StatelessWidget {
+  const BottomActionBar({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Row(
