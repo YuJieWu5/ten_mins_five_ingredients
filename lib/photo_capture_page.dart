cb33b8ffe13d5e114164e3270acdd7b1b01bb6ea
ivyw92 <swang13@seattleu.edu>
2024-02-04 23:02:04 -0800

    Create photo_capture_page.dart

---
+++ lib/photo_capture_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class PhotoCapturePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Take a Photo'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        // Use SingleChildScrollView for scrolling
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 3/4, // Square box
+              child: Container(
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 2),
+                ),
+                child: Center(
+                  child: Text('Placeholder for camera widget'),
+                ),
+              ),
+            ),
+            ActionBar(), // Confirm to use the photo
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ActionBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: ElevatedButton(
+              child: Text('Confirm'),
+              onPressed: () {
+                // Navigate to the Ingredients list
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
