45ef4e118aee4a6e5be948f1958bb74ea412463e
YuJie Wu <wuvivian725@gmail.com>
2024-02-05 02:36:36 -0800

    Create upload_recipe_page.dart

---
+++ lib/upload_recipe_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UploadRecipePage extends StatefulWidget {
+  const UploadRecipePage({super.key});
+
+  @override
+  State<UploadRecipePage> createState() => _UploadRecipePageState();
+}
+
+class _UploadRecipePageState extends State<UploadRecipePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Upload Recipe Page');
+  }
+}
