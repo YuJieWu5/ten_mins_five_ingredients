b433926da19cbd8c68513953f7f9872ea67c30d4
ivyw92 <swang13@seattleu.edu>
2024-02-05 02:27:27 -0800

    update homepage actions

--- lib/homepage_bottom_action_bar.dart
+++ lib/homepage_bottom_action_bar.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'photo_capture_page.dart';
-import 'recipe_list_page.dart';
 import 'package:go_router/go_router.dart';
 
 class BottomActionBar extends StatelessWidget {
@@ -9,38 +7,20 @@ class BottomActionBar extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
-        Flexible(
-          child: ElevatedButton(
-          child: Text('📷 Get Recipe'),
-          onPressed: () => context.go('/getRecipe')
-        )
+        ElevatedButton(
+            child: Text('Get Recipe'),
+            onPressed: () => context.go('/getRecipe')),
+        IconButton(
+          icon: Icon(Icons.account_circle),
+          onPressed: () {
+            // TODO: Placeholder to navigate to profile
+          },
         ),
-        Flexible(
-          child: ElevatedButton(
-            child: Text('View Recipe'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => RecipeList()),
-              );
-            },
-          ),
-        ),
-        Flexible(
-          child: ElevatedButton(
-            child: Text('Create Recipe'),
-            onPressed: () {
-              // TODO: Placeholder to implement create function
-            },
-          ),
-        ),
-        Flexible(
-          child: IconButton(
-            icon: Icon(Icons.account_circle),
-            onPressed: () {
-              // TODO: Placeholder to navigate to profile
-            },
-          ),
+        ElevatedButton(
+          child: Text('Create Recipe'),
+          onPressed: () {
+            // TODO: Placeholder to implement create function
+          },
         ),
       ],
     );
