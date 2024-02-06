86599d306eecafa13ed9dea9d48b29ffcd599461
ivyw92 <swang13@seattleu.edu>
2024-02-06 01:10:29 -0800

    Create Recipe detailed test

---
+++ test/recipe_detail_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ten_mins_five_ingredients/recipe_detail.dart';
+import 'package:ten_mins_five_ingredients/main.dart';
+import 'package:ten_mins_five_ingredients/homepage_carousel_with_indicator.dart';
+
+
+void main() {
+  testWidgets('Recipe_detail test', (WidgetTester tester) async {
+    tester.binding.window.physicalSizeTestValue = Size(1080, 1920);
+    tester.binding.window.devicePixelRatioTestValue = 1.0;
+    await tester.pumpWidget(MaterialApp(
+      home: RecipeDetail(),
+    ));
+
+    // Verify that our counter starts at 0.¬
+    expect(find.byType(RecipeDetail), findsOneWidget);
+    expect(find.text('Ingredients'), findsOneWidget);
+    expect(find.text('Instruction'), findsOneWidget);
+    expect(find.byIcon(Icons.home), findsOneWidget);
+    expect(find.byIcon(Icons.bookmark_border_rounded), findsOneWidget);
+
+  });
+}
