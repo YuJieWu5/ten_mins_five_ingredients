3cdfe279548470aa711fdb18fbfe1d376432784e
ivyw92 <swang13@seattleu.edu>
2024-02-06 01:10:35 -0800

    minor change

--- test/login_page_test.dart
+++ test/login_page_test.dart
@@ -5,7 +5,7 @@ import 'package:ten_mins_five_ingredients/main.dart';
 import 'package:ten_mins_five_ingredients/log_in.dart';
 
 void main() {
-  testWidgets('Homepage test', (WidgetTester tester) async {
+  testWidgets('Login_page test', (WidgetTester tester) async {
     tester.binding.window.physicalSizeTestValue = Size(1080, 1920);
     tester.binding.window.devicePixelRatioTestValue = 1.0;
     await tester.pumpWidget(const MyApp());
