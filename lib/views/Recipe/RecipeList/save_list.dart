9b8ecf57f32b6a4adcc7d24cf57e1bdb0b399c7a
YuJie Wu <wuvivian725@gmail.com>
2024-02-28 14:21:36 -0800

    Connect save list with recipe detail

--- lib/views/Recipe/RecipeList/save_list.dart
+++ lib/views/Recipe/RecipeList/save_list.dart
@@ -87,7 +87,8 @@ class _SaveListState extends State<SaveList> {
                 trailing: IconButton(
                   key: Key(item['title']),
                   icon: const Icon(Icons.chevron_right),
-                  onPressed: () => GoRouter.of(context).push('/recipeDetail'),
+                  onPressed: () => GoRouter.of(context).push('/recipeDetail', extra: item),
+                  // onPressed: () => GoRouter.of(context).push('/recipeDetail'),
                 ),
                 isThreeLine: true,
               )).toList(),
