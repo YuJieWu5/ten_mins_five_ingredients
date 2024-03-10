f567df195b1117353fa48078e1a4518190df14c5
YuJie Wu <wuvivian725@gmail.com>
2024-03-09 20:52:14 -0800

    Add mock data for testing file

--- test/recipe_detail_test.dart
+++ test/recipe_detail_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ten_mins_five_ingredients/core/models/recipe.dart';
 import 'package:ten_mins_five_ingredients/views/Home/recipe_detail.dart';
 
 
@@ -8,8 +9,28 @@ void main() {
   testWidgets('Recipe_detail test', (WidgetTester tester) async {
     tester.binding.window.physicalSizeTestValue = Size(1080, 1920);
     tester.binding.window.devicePixelRatioTestValue = 1.0;
+    Map<String, dynamic> mock_recipe = {
+      "id": "-NrhK-N-i3cg1XRGBZ-K",
+      "creator": "qktJ4LO3LRLZdHWg271z",
+      "image": "Simply-Recipes-Bruschetta-Tomato-Basil-LEAD-3-772fd11de4144552a485af87f7033bf8.jpeg",
+      "ingredient": [
+        "tomato 50g",
+        "fresh basil 5g",
+        "garlic 10g",
+        "baguette slices 4 pieces"
+      ],
+      "instruction": [
+        "Combine diced tomatoes, fresh basil, garlic, olive oil, balsamic vinegar, salt and pepper",
+        "Then spoon the misture onto toasted baguette slices"
+      ],
+      "rating": 3.5,
+      "rating-count": 2,
+      "title": "Tomato Basil Bruschetta"
+    };
+
+    final Recipe recipe = Recipe.fromJson(mock_recipe);
     await tester.pumpWidget(MaterialApp(
-      home: RecipeDetail(),
+      home: RecipeDetail(recipe: recipe),
     ));
 
     // Verify that our counter starts at 0.
--- test/recipe_list_page_test.dart
+++ test/recipe_list_page_test.dart
@@ -1,11 +1,33 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:ten_mins_five_ingredients/core/models/recipe.dart';
 import 'package:ten_mins_five_ingredients/views/Recipe/RecipeList/recipe_list_page.dart';
 import 'package:ten_mins_five_ingredients/views/Home/recipe_detail.dart';
 
 main(){
   testWidgets("Test recipe list page ui and navigation", (WidgetTester tester) async {
+    Map<String, dynamic> mock_recipe = {
+      "id": "-NrhK-N-i3cg1XRGBZ-K",
+      "creator": "qktJ4LO3LRLZdHWg271z",
+      "image": "Simply-Recipes-Bruschetta-Tomato-Basil-LEAD-3-772fd11de4144552a485af87f7033bf8.jpeg",
+      "ingredient": [
+        "tomato 50g",
+        "fresh basil 5g",
+        "garlic 10g",
+        "baguette slices 4 pieces"
+      ],
+      "instruction": [
+        "Combine diced tomatoes, fresh basil, garlic, olive oil, balsamic vinegar, salt and pepper",
+        "Then spoon the misture onto toasted baguette slices"
+      ],
+      "rating": 3.5,
+      "rating-count": 2,
+      "title": "Tomato Basil Bruschetta"
+    };
+
+    final Recipe recipe = Recipe.fromJson(mock_recipe);
+
     final GoRouter router = GoRouter(
       routes: [
         GoRoute(
@@ -14,7 +36,7 @@ main(){
         ),
         GoRoute(
           path: '/recipeDetail',
-          builder: (BuildContext context, GoRouterState state) => RecipeDetail(),
+          builder: (BuildContext context, GoRouterState state) => RecipeDetail(recipe: recipe),
         )
       ],
     );
